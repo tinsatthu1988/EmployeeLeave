a3a76f11d3385f853d936ba59d1b3054cac666a4
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-01-20 13:06:19 +0700

    008 Setup Database, Models and Migrations

---
+++ EmployeeLeave/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeLeave.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeLeave.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+    }
+}
