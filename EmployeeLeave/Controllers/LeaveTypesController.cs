544660718c96d6a6afa707e24cacb45d74ef5400
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-01-21 15:46:16 +0700

    020 Setup Delete Function

--- EmployeeLeave/Controllers/LeaveTypesController.cs
+++ EmployeeLeave/Controllers/LeaveTypesController.cs
@@ -123,21 +123,42 @@ namespace EmployeeLeave.Controllers
         // GET: LeaveTypesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leavetype = _repo.FindById(id);
+            if (leavetype == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _repo.Delete(leavetype);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveTypeVM model)
         {
             try
             {
+                // TODO: Add delete logic here
+                var leavetype = _repo.FindById(id);
+                if (leavetype == null)
+                {
+                    return NotFound();
+                }
+                var isSuccess = _repo.Delete(leavetype);
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
