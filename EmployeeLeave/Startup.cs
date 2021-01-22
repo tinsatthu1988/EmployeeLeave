a89c3020d6540e9072b5ea7ae3232ba8457815de
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-01-22 14:29:35 +0700

    026 Add Default Roles and Admin User

--- EmployeeLeave/Startup.cs
+++ EmployeeLeave/Startup.cs
@@ -43,13 +43,19 @@ namespace EmployeeLeave
             services.AddAutoMapper(typeof(Maps));
 
             services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+        )
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +75,8 @@ namespace EmployeeLeave
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            SeedData.Seed(userManager, roleManager);
 
             app.UseEndpoints(endpoints =>
             {
