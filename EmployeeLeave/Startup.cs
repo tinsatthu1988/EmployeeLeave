29d32c12ea4efd28289b0bb6bb911370f07566c3
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-01-22 11:38:46 +0700

    024 Customizing Registration Page and Process

--- EmployeeLeave/Startup.cs
+++ EmployeeLeave/Startup.cs
@@ -42,7 +42,7 @@ namespace EmployeeLeave
 
             services.AddAutoMapper(typeof(Maps));
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
