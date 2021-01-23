47a1d0b0bb326b0e02a05f75e4c7b271a7319876
tin <tinsatthu1988@gmail.com>
2021-01-24 00:21:40 +0700

    042 Employee Application View Part 2

--- EmployeeLeave/Controllers/LeaveRequestController.cs
+++ EmployeeLeave/Controllers/LeaveRequestController.cs
@@ -80,15 +80,67 @@ namespace EmployeeLeave.Controllers
         // POST: LeaveRequestController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(CreateLeaveRequestVM model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var startDate = Convert.ToDateTime(model.StartDate);
+                var endDate = Convert.ToDateTime(model.EndDate);
+                var leaveTypes = _leaveTypeRepo.FindAll();
+                var leaveTypeItems = leaveTypes.Select(q => new SelectListItem
+                {
+                    Text = q.Name,
+                    Value = q.Id.ToString()
+                });
+                model.LeaveTypes = leaveTypeItems;
+
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                if (DateTime.Compare(startDate, endDate) > 1)
+                {
+                    ModelState.AddModelError("", "Start Date cannot be further in the future than the End Date");
+                    return View(model);
+                }
+
+                var employee = _userManager.GetUserAsync(User).Result;
+                var allocation = _leaveAllocRepo.GetLeaveAllocationsByEmployeeAndType(employee.Id, model.LeaveTypeId);
+                int daysRequested = (int)(endDate - startDate).TotalDays;
+
+                if (daysRequested > allocation.NumberOfDays)
+                {
+                    ModelState.AddModelError("", "You Do Not Sufficient Days For This Request");
+                    return View(model);
+                }
+
+                var leaveRequestModel = new LeaveRequestVM
+                {
+                    RequestingEmployeeId = employee.Id,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    Approved = null,
+                    DateRequested = DateTime.Now,
+                    DateActioned = DateTime.Now,
+                    LeaveTypeId = model.LeaveTypeId
+                };
+
+                var leaveRequest = _mapper.Map<LeaveRequest>(leaveRequestModel);
+                var isSuccess = _leaveRequestRepo.Create(leaveRequest);
+
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something went wrong with submitting your record");
+                    return View(model);
+                }
+
+                return RedirectToAction("MyLeave");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ModelState.AddModelError("", "Something went wrong");
+                return View(model);
             }
         }
 
--- EmployeeLeave/Models/LeaveRequestVM.cs
+++ EmployeeLeave/Models/LeaveRequestVM.cs
@@ -11,21 +11,33 @@ namespace EmployeeLeave.Models
     public class LeaveRequestVM
     {
         public int Id { get; set; }
-
         public EmployeeVM RequestingEmployee { get; set; }
+        [Display(Name = "Employee Name")]
         public string RequestingEmployeeId { get; set; }
+        [Display(Name = "Start Date")]
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
-
         public LeaveTypeVM LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
 
+        [Display(Name = "Date Requested")]
         public DateTime DateRequested { get; set; }
+        [Display(Name = "Date Actioned")]
         public DateTime DateActioned { get; set; }
+        [Display(Name = "Approval State")]
         public bool? Approved { get; set; }
-
         public EmployeeVM ApprovedBy { get; set; }
+        [Display(Name = "Approver Name")]
         public string ApprovedById { get; set; }
+        public bool Cancelled { get; set; }
+        [Display(Name = "Employee Comments")]
+        [MaxLength(300)]
+        public string RequestComments { get; set; }
     }
 
     public class AdminLeaveRequestViewVM
